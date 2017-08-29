4283e7e063c14a91ce6d3e7c4fc7bac1ad4205f4
AlekseySugrobov <Aleksey.Sugrobov@gmail.com>
2017-08-29 14:37:27 +0500

    100% tests coverage

--- FizzBuzz.Library.Tests/FizzBuzzerTests.cs
+++ FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -7,13 +7,7 @@ namespace FizzBuzz.Library.Tests
     [TestFixture]
     public class FizzBuzzerTests
     {
-        [Test]
-        public void CheckNUnitFrameworkIsWorks()
-        {
-            Assert.Pass();
-        }
-        
-        [Test] public void Buzzer_WhenDefault_ReturnsInput(
+       [Test] public void Buzzer_WhenDefault_ReturnsInput(
             [Values(1,2,4,7,8,11,13,14,16,17,19)]int input)
         {
             var output = FizzBuzzer.GetValue(input);
